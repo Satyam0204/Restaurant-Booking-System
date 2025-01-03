6cf11a4b6f9b31a9883b98f5f3baf7392726007a
Satyam0204 <satyampattanaik007@gmail.com>
2025-01-03 19:48:27 +0530

    Added Validation for contact

--- backend/src/models/Booking.js
+++ backend/src/models/Booking.js
@@ -5,7 +5,16 @@ const bookingSchema = new mongoose.Schema({
   timeSlot: { type: String, required: true }, 
   guests: { type: Number, required: true },
   name: { type: String, required: true },
-  contact: { type: String, required: true },
+  contact: { 
+    type: String, 
+    required: true, 
+    validate: {
+      validator: function (v) {
+        return /^\+?[1-9]\d{1,14}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`
+    }
+  },
   tableNumber: [{ type: Number, required: true }],
 });
 
