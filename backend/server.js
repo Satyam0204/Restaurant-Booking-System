1f5e0ac158cf056e6fa1e518160834f65fb97168
Satyam0204 <satyampattanaik007@gmail.com>
2025-01-03 17:50:30 +0530

    Deploy on render

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const dotenv = require("dotenv");
-const connectDB = require("../src/config/db");
-const app = require("../src/app");
+const connectDB = require("./src/config/db");
+const app = require("./src/app");
+
+
 
 dotenv.config();
 
-// Connect to MongoDB
+
 connectDB();
 
 app.get("/", (req, res) => {
@@ -15,3 +17,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
